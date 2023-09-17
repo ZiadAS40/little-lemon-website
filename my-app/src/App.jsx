67b12e8d6565e7f587d2793091fd2afc5f1f6dd3
ZiadAS40 <ziad403612@gmail.com>
2023-09-17 18:54:55 +0300

    the main structure

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,16 @@
 
 import './App.css'
+import Header from './assets/Header'
+import Main from './assets/Main'
+import Footer from './assets/Footer'
 
 function App() {
 
   return (
    <>
+   <Header/>
+   <Main/> 
+   <Footer/>
    </>
   )
 }
---
+++ my-app/src/assets/Header.js
@@ -0,0 +1,15 @@
+import Logo from "./assets/Logo"
+import Nav from "./assets/Nav"
+
+function Header(){
+    return (
+        <>
+        <Logo/>
+        <Nav/>
+
+        </>
+    )
+}
+
+
+export default Header
