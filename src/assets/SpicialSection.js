8c40a74de293819f712ad2dcba8d9c046e7a383d
ZiadAS40 <ziad403612@gmail.com>
2023-09-21 17:19:25 +0300

    saving the project for learning

--- src/assets/SpicialSection.js
+++ src/assets/SpicialSection.js
@@ -1,7 +1,36 @@
+let dishes = {
+    GreekSalaad : {
+ tittle : GreekSalaad,
+ dicreption: "The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. ",
+ getImage: ()=> require('../icons_assets/greek salad.svg')
+    },
+    Brucheta : {
+        tittle : Brucheta,
+        dicreption: "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.  ",
+        getImage: ()=> require('../icons_assets/bruchetta.svg')
+    },   
+    lemonDessert : {
+        tittle : LemonDessert,
+        dicreption: "The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. ",
+        getImage: ()=> require('../icons_assets/lemon dessert.svg')
+    }
+}
+
 function SpecialSection(){
+    dishes.map(()=>(
+
+    ))
     return(
         <>
-        
+        <section>
+            <div className="the-above-sec">
+                <h1>This weak Specials</h1>
+                <button>Online Menu</button>
+            </div>
+            <div className="tha-dishes">
+                
+            </div>
+        </section>
         </>
     )
 }
